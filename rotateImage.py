5dc1f80db67d9c07d112f16f49175d10767b062a
osmya <45291785+osmya@users.noreply.github.com>
2019-06-25 16:28:12 +0100

    Update rotateImage.py

--- rotateImage.py
+++ rotateImage.py
@@ -16,3 +16,36 @@ def rotateImage(a):
         start += 1
         unitmin += 1
     return rotated
+
+
+# alternative solution (create tuples?) *** should use list in front
+
+a = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
+rotateImage = lambda a: zip(*a[::-1]) # reverses a then zip
+sol = rotateImage(a)
+>>> sol
+[(13, 9, 5, 1), (14, 10, 6, 2), (15, 11, 7, 3), (16, 12, 8, 4)]
+
+
+# alternative solution using reverse method
+
+def rotateImage(a):
+    a.reverse()
+    for i in range(len(a)):
+        for j in range(i):
+            a[i][j], a[j][i] = a[j][i], a[i][j]
+    return a
+
+# alternative solution with zip
+
+def rotateImage(a):
+    return list(zip(*reversed(a)))
+# check this for *func https://stackoverflow.com/questions/13635032/what-is-the-inverse-function-of-zip-in-python
+
+""" When used like this, the * (asterisk, also know in some circles as the "splat" operator) is a signal to unpack 
+arguments from a list. See http://docs.python.org/tutorial/controlflow.html#unpacking-argument-lists
+for a more complete definition with examples.
+
+https://stackoverflow.com/questions/5917522/unzipping-and-the-operator
+"""
+
