42f0ea276b6d25549fcc01d424641a85a8f55ab2
osmya <45291785+osmya@users.noreply.github.com>
2019-06-26 23:22:31 +0100

    Create century_from_year.py

---
+++ century_from_year.py
@@ -0,0 +1,8 @@
+def centuryFromYear(year):
+    breaks = [range(i, i+101) for i in range(0, 2100, 100)]
+    centuries = {c+1:i for c,i in enumerate(breaks)}
+    def match(y):
+        for item in centuries.items():
+            if year in item[1]:
+                return item[0]
+    return match(year)
