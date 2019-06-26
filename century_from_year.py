d7bc7aa9ea59751b515ca318009a537895268fdc
osmya <45291785+osmya@users.noreply.github.com>
2019-06-26 23:24:45 +0100

    Update century_from_year.py

--- century_from_year.py
+++ century_from_year.py
@@ -1,3 +1,9 @@
+# simple solution with //
+def centuryFromYear(year):
+    return (year + 99) // 100
+
+# or some list and dict comprehension
+
 def centuryFromYear(year):
     breaks = [range(i, i+101) for i in range(0, 2100, 100)]
     centuries = {c+1:i for c,i in enumerate(breaks)}
@@ -6,3 +12,5 @@ def centuryFromYear(year):
             if year in item[1]:
                 return item[0]
     return match(year)
+
+
