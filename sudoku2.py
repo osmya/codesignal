6911e827d43058b6de0e2bb222e9e608208d099a
osmya <45291785+osmya@users.noreply.github.com>
2019-06-28 13:33:22 +0100

    Update sudoku2.py

--- sudoku2.py
+++ sudoku2.py
@@ -74,3 +74,97 @@ def sudoku2(a):
     row_check(rn, ai)
     col_check(cn, bi)
     return count <= 0
+
+
+
+# putting it all together...
+
+def sudoku2(a):
+    b = list(zip(*reversed(a)))
+    rows = [set() for i in a]
+    cols = [set() for i in b]
+    r_iter = iter(rows)
+    c_iter = iter(cols)
+    a_iter = iter(a)
+    b_iter = iter(b)
+    ai = next(a_iter)
+    bi = next(b_iter)
+    rn = next(r_iter)
+    cn = next(c_iter)
+    grids = [[x[i] for x in a[0:3] for i in range(3)],
+    [x[i] for x in a[3:6] for i in range(3)],
+    [x[i] for x in a[6:9] for i in range(3)],
+    [x[i] for x in a[0:3] for i in range(3, 6)],
+    [x[i] for x in a[3:6] for i in range(3, 6)],
+    [x[i] for x in a[6:9] for i in range(3, 6)],
+    [x[i] for x in a[0:3] for i in range(6, 9)],
+    [x[i] for x in a[3:6] for i in range(6, 9)],
+    [x[i] for x in a[6:9] for i in range(6, 9)]]
+    sets = [set() for i in grids]
+    s_iter = iter(sets)
+    gd_iter = iter(grids)
+    s1 = next(s_iter)
+    gd1 = next(gd_iter)
+    result = []
+    count = 0
+    def row_check(r, y):
+        nonlocal count
+        k = 0
+        while k <= len(y):
+            for i in y:
+                if i == '.':
+                    count += 0
+                elif i in r:
+                    count += 1
+                else:
+                    r.add(i)
+            k += 1
+            r = next(r_iter, None)
+            if r is None:
+                pass
+            y = next(a_iter, None)
+            if y is None:
+                return count
+    def col_check(c, x):
+        nonlocal count
+        g = 0
+        while g <= len(x):
+            for i in x:
+                if i == '.':
+                    count += 0
+                elif i in c:
+                    count += 1
+                else:
+                    c.add(i)
+            g += 1
+            c = next(c_iter, None)
+            if c is None:
+                pass
+            x = next(b_iter, None)
+            if x is None:
+                return count
+    def grid_check(s, gd):
+        nonlocal grids
+        nonlocal result
+        k = 0
+        while k <= len(grids):
+            for i in gd:
+                if i == '.':
+                    result += [0]
+                elif i in s:
+                    result += [1]
+                    return result
+                else:
+                    s.add(i)
+            k += 1
+            s = next(s_iter, None)
+            if s is None:
+                pass
+            gd = next(gd_iter, None)
+            if gd is None:
+                return result
+    grid_check(s1, gd1)
+    row_check(rn, ai)
+    col_check(cn, bi)
+    return 1 not in result or count <= 0
+
