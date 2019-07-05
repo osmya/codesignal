71814c08a7d87496766c7d00d2239fc113feefee
osmya <45291785+osmya@users.noreply.github.com>
2019-07-05 16:03:45 +0100

    Update pairwise.py

--- pairwise.py
+++ pairwise.py
@@ -2,13 +2,18 @@ def adjacentElementsProduct(a):
     k = [x*y for x, y in zip(a[1:], a)]
     return max(k)
 
-# can do
+# cleaner
 
 def adjacentElementsProduct(a):
     return max([x*y for x, y in zip(a[1:], a)])
     
 # or directly max(x*y for x, y in zip(a[1:], a))
 
+
+# other approach
+
+def adjacentElementsProduct(a):
+    return max([a[i] * a[i+1] for i in range(len(a)-1)])
 
 
 
