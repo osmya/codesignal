a50a5b9329e2f4e49fa03a817508a4e1bc0850f0
osmya <45291785+osmya@users.noreply.github.com>
2019-07-15 13:22:31 +0100

    Update allLongestStrings.py

--- allLongestStrings.py
+++ allLongestStrings.py
@@ -1,3 +1,9 @@
 def allLongestStrings(a):
     mark = max([len(i) for i in a])
     return [i for i in a if len(i) == mark]
+
+
+# with map and filter
+def allLongestStrings(a):
+    m = max(map(len, a))
+    return filter(lambda x: len(x) == m, a)
